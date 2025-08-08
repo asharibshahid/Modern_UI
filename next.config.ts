910893cb52f99103c07f6841a3914c8f8ea7c26a
ASHARIB-SHEIKH👩🏻‍💻 <160227183+asharibshahid@users.noreply.github.com>
2025-08-08 12:45:34 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+
+
+    domains: ['res.cloudinary.com' , 'ucarecdn.com'], // ✅ Cloudinary allow
+  },
 };
 
 export default nextConfig;
